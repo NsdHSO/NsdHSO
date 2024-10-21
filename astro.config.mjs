28c1989c8bab84bf3e23ed8706e2d1adad93aee4
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2024-10-21 18:04:54 +0300

    chore:🌻 added base to be

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://github.com/nsdhso.github.io',
-	base: 'my-repo',
+	base: '',
 	integrations: [mdx(), sitemap()],
 });
