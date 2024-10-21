12d5766187671abc9996d67dab29edd097530390
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2024-10-21 18:29:42 +0300

    chore:🌻 added base for site

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://github.com/nsdhso.github.io',
+	site: 'https://github.com/nsdhso.github.io/cv',
 	base: '/cv/',
 	integrations: [mdx(), sitemap()],
 });
