5d819646f0f9a0e5f70b43140d9bd5414604c761
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2024-10-21 17:46:03 +0300

    chore:🌻 added content for github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://github.com/NsdHSO/NsdHSO',
+	base: 'my-repo',
 	integrations: [mdx(), sitemap()],
 });
